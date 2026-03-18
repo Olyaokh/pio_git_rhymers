8b80e0826ba6b95ffe6a9af98d5b4309c5c0524d
Olyaokh <258486@edu.p.lodz.pl>
2026-03-18 21:29:54 +0200

    Accessors i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,38 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
-
-    public int total = -1;
+    private final int[] numbers = new int[12];
+    private int total = -1;
 
     public void countIn(int in) {
-        if (!isFull())
+        if (!isFull()) {
             numbers[++total] = in;
+        }
     }
 
-        public boolean сallCheck() {
-            return total == -1;
-        }
-        
-            public boolean isFull() {
-                return total == 11;
-            }
-        
-                protected int peekaboo() {
-                    if (callCheck())
-                        return -1;
-                    return numbers[total];
-                }
-            
-                    public int countOut() {
-                        if (callCheck())
-                            return -1;
-                        return numbers[total--];
-                    }
+    public boolean callCheck() {
+        return total == -1;
+    }
 
+    public boolean isFull() {
+        return total == 11;
+    }
+
+    protected int peekaboo() {
+        if (callCheck()) {
+            return -1;
+        }
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck()) {
+            return -1;
+        }
+        return numbers[total--];
+    }
+
+    public int getTotal() {
+        return total;
+    }
 }
