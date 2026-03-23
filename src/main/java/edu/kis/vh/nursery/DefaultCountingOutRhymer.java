b3ab311da25b7d33a2c2d2f939f7cd2962e1bd2b
Jakub Osniecki <255669@edu.p.lodz.pl>
2026-03-23 13:13:06 +0100

    Ostateczne poprawki - utworzenie zmiennych NUMBERS_CAPACITY, EMPTY_STACK_VALUE, DEFAULT_RETURN

---
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -0,0 +1,37 @@
+package edu.kis.vh.nursery;
+
+public class DefaultCountingOutRhymer {
+
+    public static final int NUMBERS_CAPACITY = 12;
+    public static final int EMPTY_STACK_VALUE = -1;
+    public static final int DEFAULT_RETURN = -1;
+
+    private final int[] numbers = new int[NUMBERS_CAPACITY];
+
+    public int total = EMPTY_STACK_VALUE;
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == EMPTY_STACK_VALUE;
+    }
+
+    public boolean isFull() {
+        return total == NUMBERS_CAPACITY - 1;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return DEFAULT_RETURN;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return DEFAULT_RETURN;
+        return numbers[total--];
+    }
+}
